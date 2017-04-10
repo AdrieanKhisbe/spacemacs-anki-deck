51c39f5176aac52ad762f85ab9d5e12f9875e2ee
AdrieanKhisbe <adriean.khisbe@live.fr>
2017-04-10 23:17:52 +0200

    draft of a generator of anki from bindings definitions

--- index.js
+++ index.js
@@ -6,8 +6,24 @@ const apkg = new AnkiExport('spacemacs-binding');
 //apkg.addMedia('anki.png', fs.readFileSync('anki.png'));
 
 apkg.addCard('card #1 front', 'card #1 back');
-apkg.addCard('card #2 front', 'card #2 back', { tags: ['nice', 'better card'] });
+apkg.addCard('card #2 front', 'card #2 back', {tags: ['nice', 'better card']});
 // apkg.addCard('card #3 with image <img src="anki.png" />', 'card #3 back');
+
+let categories = ['search'];
+
+categories.forEach((cat) => {
+    const catContent = fs.readFileSync(`./bindings/${cat}`)
+    catContent.toString()
+        .split('\n')
+        .map((line) => {
+            const sides = line.split(' - ');
+            apkg.addCard(sides[0], sides[1], {tags: [cat, 'binding to def']})
+            apkg.addCard(sides[1], sides[0], {tags: [cat, 'def to binding']})
+        })
+})
+
+// maybe: option two sided
+
 
 apkg
     .save()

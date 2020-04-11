bc8069ce835861d6a5b891febac045f38c0e9f4f
AdrieanKhisbe <adriean.khisbe@live.fr>
2020-04-11 19:06:12 +0200

    Turn package into command :rocket:

--- README.md
+++ README.md
@@ -1,16 +1,17 @@
 # Spacemacs Anki Deck
 
-Basic [spacemacs](http://spacemacs.org) [Anki](https://apps.ankiweb.net/) deck generator.
+> Basic [spacemacs](http://spacemacs.org) [Anki](https://apps.ankiweb.net/) deck generator
 
+[![Npm](https://img.shields.io/node/v/spacemacs-anki-deck.svg?logo=node.js)](https://www.npmjs.com/package/spacemacs-anki-deck
 
-Generate the `apkg` deck with the following commands:
-```sh
-  node index.js
-```
+To Generate the `apkg` deck you can either:
+- install `spacemacs-anki-deck` globally and run `spacemacs-anki-deck some-folder`
+- use `npx` with `npx spacemacs-anki-deck some-folder`
+- using source: run `npm start` from source folder, and provide some folder to generate
+
 
 Cards definitions are in the [mappings folder](./mappings)
 
 A first deck is available in the [release section](https://github.com/AdrieanKhisbe/spacemacs-anki-deck/releases/tag/v0.1.0).
-
 
 **Please note**: I'm not really using spacemacs that right now, having changed editor. But feel free to open a PR to update bindings if you ned
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+#!/usr/bin/env node
 const fs = require('fs');
+const path = require('path');
 const AnkiExport = require('anki-apkg-export').default;
 
 const categories = [
@@ -14,29 +16,40 @@ const categories = [
   'toggle'
 ];
 
-if (!fs.existsSync('output')) fs.mkdirSync('output');
+const generate = folder => {
+  if (!fs.existsSync(folder)) fs.mkdirSync(folder);
 
-categories.forEach(cat => {
-  const apkg = new AnkiExport(`spacemacs-bindings::${cat}`);
-  const catContent = fs.readFileSync(`./mappings/${cat}`, 'utf-8');
-  catContent.split('\n').map(line => {
-    const sides = line.split(' -- ');
-    if (sides.length === 2) {
-      const keys = `<kbd>${sides[0]}</kbd>`.replace('SPC', '<i>SPC</i>');
+  categories.forEach(cat => {
+    const apkg = new AnkiExport(`spacemacs-bindings::${cat}`);
+    const catContent = fs.readFileSync(path.join(__dirname, `mappings/${cat}`), 'utf-8');
+    catContent.split('\n').map(line => {
+      const sides = line.split(' -- ');
+      if (sides.length === 2) {
+        const keys = `<kbd>${sides[0]}</kbd>`.replace('SPC', '<i>SPC</i>');
 
-      apkg.addCard(keys, sides[1], {tags: [cat, 'binding to def']});
-      // apkg.addCard(sides[1], keys, {tags: [cat, 'def to binding']})
-      // maybe: option two sided -> would need to shuffle the cards
+        apkg.addCard(keys, sides[1], {tags: [cat, 'binding to def']});
+        // apkg.addCard(sides[1], keys, {tags: [cat, 'def to binding']})
+        // maybe: option two sided -> would need to shuffle the cards
 
-      // §idea: extra args to add tags --
-    }
+        // §idea: extra args to add tags --
+      }
+    });
+
+    apkg
+      .save()
+      .then(zip => {
+        fs.writeFileSync(path.join(folder, `spacemacs-${cat}.apkg`), zip, 'binary');
+        console.log(`Package has been generated: spacemacs-${cat}.apkg`);
+      })
+      .catch(err => console.log(err.stack || err));
   });
-
-  apkg
-    .save()
-    .then(zip => {
-      fs.writeFileSync(`./output/spacemacs-${cat}.apkg`, zip, 'binary');
-      console.log(`Package has been generated: spacemacs-${cat}.apkg`);
-    })
-    .catch(err => console.log(err.stack || err));
-});
+};
+module.exports = {generate};
+if (!module.parent) {
+  const folder = process.argv[2];
+  if (!folder) {
+    console.error('Provide a folder to generate deck in');
+    process.exit(1);
+  }
+  generate(folder);
+}
